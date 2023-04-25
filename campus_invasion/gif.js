119eba12ddfe88cddc132156ce9a4de93a5aa201
Kevin Gallardo <andreskevin2606@gmail.com>
2023-04-24 20:45:21 -0500

    final

--- campus_invasion/gif.js
+++
@@ -1,8 +0,0 @@
-let img = document.getElementById("img");
-
-img.addEventListener("mouseover", ()=>{
-    img.src = "img/BjlN.gif"
-})
-img.addEventListener("mouseout", ()=>{
-    img.src = "img/doom.jpg"
-})
